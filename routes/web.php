f3a9ccdfad5b29659325f58fda2348178ec6ae64
Yousef Khrais <yousefokhrais@gmail.com>
2022-07-01 09:41:57 +0300

    Cleaned routes file --Web.php

--- routes/web.php
+++ routes/web.php
@@ -12,80 +12,62 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-//home pages
 
-Route::get('/', [\App\Http\Controllers\HomeController::class,"index"])->name("home");
-Route::get('/about', [\App\Http\Controllers\HomeController::class,"about"])->name("about");
-Route::get('/trainers', [\App\Http\Controllers\Teacher\TeachersController::class,"index"])->name("trainers");
-Route::get('/pricing', [\App\Http\Controllers\HomeController::class,"pricing"])->name("pricing");
-Route::get('/contact', [\App\Http\Controllers\HomeController::class,"contact"])->name("contact");
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Teacher\TeachersController;
+use App\Http\Controllers\Teacher\TeacherLoginController;
+use App\Http\Controllers\Teacher\TeacherProfileController;
+use App\Http\Controllers\Student\SessionController;
+use App\Http\Controllers\Student\DashBoard;
+use App\Http\Controllers\Student\ProfileController;
+use App\Http\Controllers\Student\LoginController;
+use App\Http\Controllers\Student\RegisterController;
+use App\Http\Controllers\Categories\Categories;
+use App\Http\Controllers\Courses\CoursesController;
+use App\Http\Controllers\Rooms\RoomController;
 
-////end home routes
-//Route::group(['prefix' => 'admin'], function () {
-//    Voyager::routes();
-//});
+Route::get('/', [HomeController::class, "index"])->name("home");
+Route::get('/about', [HomeController::class, "about"])->name("about");
+Route::get('/trainers', [TeachersController::class, "index"])->name("trainers");
+Route::get('/pricing', [HomeController::class, "pricing"])->name("pricing");
+Route::get('/contact', [HomeController::class, "contact"])->name("contact");
 
-//Auth::routes();
+Route::prefix('/student')->group(function () {
+    Route::get('/', [DashBoard::class, 'index'])->name('studentDashboard');
+    Route::get("/session", [SessionController::class, "index"]);
+    Route::get("/profile", [ProfileController::class, "index"])->name("StudentProfile")->middleware("auth:student");
+    Route::get("/profile/edit", [ProfileController::class, "editProfilePage"])->name("StudentEditProfile")->middleware("auth:student");
+    Route::get("/login", [LoginController::class, "index"])->name("studentLogin");
+    Route::get("/logout", [LoginController::class, "logout"])->name("studentLogout");
+    Route::get("/register", [RegisterController::class, "index"])->name("StudentRegister");
+    Route::post("/login", [LoginController::class, "login"])->name("studentLogin");
+    Route::post("/profile/edit", [ProfileController::class, "updateStudent"])->name("updateStudent")->middleware("auth:student");
+    Route::post("/register", [RegisterController::class, "register"])->name("StudentRegister");
+});
 
-//Student
-Route::get('/student/', [App\Http\Controllers\Student\DashBoard::class, 'index'])->name('studentDashboard');
-Route::get('/categories', [App\Http\Controllers\Categories\Categories::class, 'index'])->name('categories');
-Route::get("/student/session",[\App\Http\Controllers\Student\SessionController::class,"index"]);
+Route::prefix('/categories')->group(function () {
+    Route::get('/', [Categories::class, 'index'])->name('categories');
+    Route::get("/{category}", [Categories::class, 'courses'])->name("categoryCourses");
+});
 
-Route::get("/categories/{category}",[App\Http\Controllers\Categories\Categories::class, 'courses'])->name("categoryCourses");
+Route::prefix('/courses')->group(function () {
+    Route::get('/', [CoursesController::class, "index"])->name("courses");
+    Route::get('/{course}', [CoursesController::class, "show"])->name("courseDetails");
+    Route::post("/entrollCourse", [CoursesController::class, "entroll"])->name("entrollCourse")->middleware("auth:student");
+});
 
-Route::get("/student/profile",[\App\Http\Controllers\Student\ProfileController::class,"index"])
-    ->name("StudentProfile")->middleware("auth:student");
+Route::prefix('/teacher')->group(function () {
+    Route::get("/login", [TeacherLoginController::class, "index"])->name("teacherLogin");
+    Route::get("/logout", [TeacherLoginController::class, "logout"])->name("teacherLogOut");
+    Route::get("/getCourses", [TeachersController::class, "getTeacherCourses"])->name("teacher_courses")->middleware("auth:teacher");
+    Route::get("/profile", [TeacherProfileController::class, "index"])->name("teacherProfile")->middleware("auth:teacher");
+    Route::get("/profile/edit", [TeacherProfileController::class, "editProfile"])->name("editTeacherProfile")->middleware("auth:teacher");
+    Route::post("/profile/edit", [TeacherProfileController::class, "updateTeacher"])->name("updateTeacher")->middleware("auth:teacher");
+    Route::post("/login", [TeacherLoginController::class, "login"])->name("teacherLogin");
+});
 
-Route::get("/student/profile/edit",[\App\Http\Controllers\Student\ProfileController::class,"editProfilePage"])
-    ->name("StudentEditProfile")->middleware("auth:student");
-
-Route::post("/student/profile/edit",[\App\Http\Controllers\Student\ProfileController::class,"updateStudent"])
-    ->name("updateStudent")->middleware("auth:student");
-
-Route::get("/student/login",[\App\Http\Controllers\Student\LoginController::class,"index"])->name("studentLogin");
-Route::post("/student/login",[\App\Http\Controllers\Student\LoginController::class,"login"])->name("studentLogin");
-
-Route::get("/student/register",[\App\Http\Controllers\Student\RegisterController::class,"index"])->name("StudentRegister");
-Route::post("/student/register",[\App\Http\Controllers\Student\RegisterController::class,"register"])->name("StudentRegister");
-
-Route::get("/student/logout",[\App\Http\Controllers\Student\LoginController::class,"logout"])->name("studentLogout");
-
-//Courses Routes
-Route::get('/courses', [\App\Http\Controllers\Courses\CoursesController::class,"index"])->name("courses");
-Route::get('/courses/{course}', [\App\Http\Controllers\Courses\CoursesController::class,"show"])->name("courseDetails");
-
-Route::post("courses/entrollCourse",[
-    \App\Http\Controllers\Courses\CoursesController::class,"entroll"
-])->name("entrollCourse")->middleware("auth:student");
-
-//teacher routes
-Route::get("teacher/login",[\App\Http\Controllers\Teacher\TeacherLoginController::class,"index"])->name("teacherLogin");
-Route::post("teacher/login",[\App\Http\Controllers\Teacher\TeacherLoginController::class,"login"])
-    ->name("teacherLogin");
-
-Route::get("teacher/logout",[\App\Http\Controllers\Teacher\TeacherLoginController::class,"logout"])->name("teacherLogOut");
-
-Route::get("teacher/profile",[\App\Http\Controllers\Teacher\TeacherProfileController::class,"index"])
-    ->name("teacherProfile")->middleware("auth:teacher");
-
-Route::get("/teacher/profile/edit",[\App\Http\Controllers\Teacher\TeacherProfileController::class,"editProfile"])
-    ->name("editTeacherProfile")->middleware("auth:teacher");
-
-Route::post("/teacher/profile/edit",[\App\Http\Controllers\Teacher\TeacherProfileController::class,"updateTeacher"])
-    ->name("updateTeacher")->middleware("auth:teacher");
-
-Route::get("teacher/getCourses",[
-    \App\Http\Controllers\Teacher\TeachersController::class,"getTeacherCourses"
-])->name("teacher_courses")->middleware("auth:teacher");
-
-//room routes
-Route::get("room/student",[\App\Http\Controllers\Rooms\RoomController::class,"studentLobby"])
-    ->name("studentLobby")->middleware("auth:student");
-
-Route::get("room/teacher",[\App\Http\Controllers\Rooms\RoomController::class,"teacherLobby"])
-    ->name("teacherLobby")->middleware("auth:teacher");
-
-Route::post("room/student/joinRoom",[
-    \App\Http\Controllers\Rooms\RoomController::class,"studentJoinRoom"
-])->name("studentJoinRoom")->middleware("auth:student");
+Route::prefix('/room')->group(function () {
+    Route::get("/student", [RoomController::class, "studentLobby"])->name("studentLobby")->middleware("auth:student");
+    Route::get("/teacher", [RoomController::class, "teacherLobby"])->name("teacherLobby")->middleware("auth:teacher");
+    Route::post("/student/joinRoom", [RoomController::class, "studentJoinRoom"])->name("studentJoinRoom")->middleware("auth:student");
+});
